5e0724c770af41a63fb848d1ac83e3f93b504756
Mauricio Rivera <mauriciorivera@protonmail.com>
2019-09-11 15:32:26 -0400

    Add move

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-// return random int
-function getRandomInt(min, max) {
+const getRandomInt = function getRandomInt(min, max) {
   const mathMin = Math.ceil(min);
   const mathMax = Math.floor(max);
   // The maximum is exclusive and the minimum is inclusive
   return Math.floor(Math.random() * (mathMax - mathMin)) + mathMin;
-}
+};
 
-function fightPokemon() {
+const displayMove = function displayMove(moveNode, moveURL, container) {
+  fetch(moveURL)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error('Network Problem');
+    })
+    .then((data) => {
+      moveNode.innerText = `${data.name} PP: ${data.pp}`;
+      container.appendChild(moveNode);
+    })
+    .catch(() => {});
+};
+
+const fightPokemon = function fightPokemon() {
   const dataArea = document.querySelector('.data');
   const pokemon = dataArea.querySelectorAll('div');
   const winner = getRandomInt(0, 2);
@@ -14,12 +28,24 @@ function fightPokemon() {
   const losingPokemon = pokemon[(winner + 1) % 2].querySelector('h3').innerText;
   const fightHistory = document.querySelector('.fightHistory');
   const titleNode = document.createElement('h5');
+  // TODO (mauricio) Bonus: use stats + moves to decide winning pokemon
   titleNode.innerText = `${winningPokemon} defeated ${losingPokemon}`;
   fightHistory.appendChild(titleNode);
-}
+};
 
-function displayRandomPokemon(pokemonID, newData) {
+const displayPokemon = function displayRandomPokemon(newData) {
+  // TODO (mauricio) Ensure pokemon IDs are unique.
+  const pokemonID = getRandomInt(1, 800);
   const topLevel = document.querySelector('body');
+  const container = document.createElement('div');
+  const nameNode = document.createElement('h3');
+  const spriteNode = document.createElement('img');
+  const hpNode = document.createElement('h5');
+  const moveTitleNode = document.createElement('h5');
+  const dataArea = document.querySelector('.data');
+  const moveIterations = [1, 2, 3, 4];
+  let moveNum;
+  let moveNode;
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
     .then((res) => {
       if (res.ok) {
@@ -28,31 +54,43 @@ function displayRandomPokemon(pokemonID, newData) {
       throw new Error('Network Problem');
     })
     .then((data) => {
-      const container = document.createElement('div');
-      const titleNode = document.createElement('h3');
       // Display name of pokemon
-      titleNode.innerText = data.name;
+      nameNode.innerText = data.name;
+      container.appendChild(nameNode);
+
       // Display sprite of pokemon
-      const elem = document.createElement('img');
-      elem.setAttribute('src', data.sprites.front_default);
-      elem.setAttribute('height', '200');
-      elem.setAttribute('width', '200');
-      container.appendChild(titleNode);
-      container.appendChild(elem);
+      spriteNode.setAttribute('src', data.sprites.front_default);
+      spriteNode.setAttribute('height', '200');
+      spriteNode.setAttribute('width', '200');
+      container.appendChild(spriteNode);
+
+      // Display hp
+      hpNode.innerText = `HP: ${data.stats[5].base_stat}`;
+      container.appendChild(hpNode);
+
+      // Display four moves
+      moveTitleNode.innerText = 'Moves:';
+      container.appendChild(moveTitleNode);
+
+      moveIterations.forEach(() => {
+        // TODO (mauricio) Ensure moves are unique.
+        moveNum = getRandomInt(0, data.moves.length);
+        moveNode = document.createElement('h5');
+        const moveURL = data.moves[moveNum].move.url;
+        displayMove(moveNode, moveURL, container);
+        // moveNode.innerText = data.moves[moveNum].move.name;
+        // container.appendChild(moveNode);
+      });
+
       newData.appendChild(container);
-      const dataArea = document.querySelector('.data');
       topLevel.replaceChild(newData, dataArea);
     })
     .catch(() => {});
-}
+};
 
-// return body of random pokemon
-function getRandomPokemon() {
-  const topLevel = document.querySelector('body');
+const getRandomPokemon = function getRandomPokemon() {
   const newData = document.createElement('div');
   newData.classList.add('data');
-  let pokemonID = getRandomInt(1, 800);
-  displayRandomPokemon(pokemonID, newData);
-  pokemonID = getRandomInt(1, 800);
-  displayRandomPokemon(pokemonID, newData);
-}
+  displayPokemon(newData);
+  displayPokemon(newData);
+};
