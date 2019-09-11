217ab40786d7c8882d21cdf5800ea36e7f7645d2
Mauricio Rivera <mauriciorivera@protonmail.com>
2019-09-11 13:04:24 -0400

    Add function for displaying pokemon

--- script.js
+++ script.js
@@ -1,79 +1,56 @@
 function fightPokemon() {
-    let topLevel = document.querySelector("body");
-    let dataArea = document.querySelector(".data");
-    let pokemons = dataArea.querySelectorAll("div");
-    winner = getRandomInt(0, 2);
-    winningPokemon = pokemons[winner].querySelector("h3").innerText;
-    losingPokemon = pokemons[(winner + 1) % 2].querySelector("h3").innerText;
-    fightLog = winningPokemon + " defeated " + losingPokemon;
-    let fightHistory = document.querySelector(".fightHistory");
-    let titleNode = document.createElement("h5");
-    titleNode.innerText = fightLog;
-    fightHistory.appendChild(titleNode);
+  let topLevel = document.querySelector("body");
+  let dataArea = document.querySelector(".data");
+  let pokemons = dataArea.querySelectorAll("div");
+  winner = getRandomInt(0, 2);
+  winningPokemon = pokemons[winner].querySelector("h3").innerText;
+  losingPokemon = pokemons[(winner + 1) % 2].querySelector("h3").innerText;
+  let fightHistory = document.querySelector(".fightHistory");
+  let titleNode = document.createElement("h5");
+  titleNode.innerText = winningPokemon + " defeated " + losingPokemon;
+  fightHistory.appendChild(titleNode);
+}
+
+function displayRandomPokemon(pokemonID, newData) {
+  let topLevel = document.querySelector("body");
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error("Network Problem");
+      }
+    })
+    .then(data => {
+      let container = document.createElement("div");
+      let titleNode = document.createElement("h3");
+      // Display name of pokemon
+      titleNode.innerText = data.name;
+      // Display sprite of pokemon
+      var elem = document.createElement("img");
+      elem.setAttribute("src", data.sprites.front_default);
+      elem.setAttribute("height", "200");
+      elem.setAttribute("width", "200");
+      container.appendChild(titleNode);
+      container.appendChild(elem);
+      newData.appendChild(container);
+      let dataArea = document.querySelector(".data");
+      topLevel.replaceChild(newData, dataArea);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 // return body of random pokemon
 function getRandomPokemon() {
-    let topLevel = document.querySelector("body");
-    let newData = document.createElement("div");
-    newData.classList.add("data");
-
-    var pokemonID = getRandomInt(1, 800);
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
-        .then(res => {
-          if (res.ok) {
-            return res.json();
-          } else {
-            throw new Error("Network Problem");
-          }
-        })
-        .then(data => {
-            container = document.createElement("div");
-            let titleNode = document.createElement("h3");
-            // Display name of pokemon
-            titleNode.innerText = data.name;
-            // Display sprite of pokemon
-            var elem = document.createElement("img");
-            elem.setAttribute("src", data.sprites.front_default);
-            elem.setAttribute("height", "200");
-            elem.setAttribute("width", "200");
-            container.appendChild(titleNode);
-            container.appendChild(elem);
-            newData.appendChild(container);
-            let dataArea = document.querySelector(".data");
-            topLevel.replaceChild(newData, dataArea);
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    pokemonID = getRandomInt(1, 800);
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
-        .then(res => {
-          if (res.ok) {
-            return res.json();
-          } else {
-            throw new Error("Network Problem");
-          }
-        })
-        .then(data => {
-            container = document.createElement("div");
-            let titleNode = document.createElement("h3");
-            // Display name of pokemon
-            titleNode.innerText = data.name;
-            // Display sprite of pokemon
-            var elem = document.createElement("img");
-            elem.setAttribute("src", data.sprites.front_default);
-            elem.setAttribute("height", "200");
-            elem.setAttribute("width", "200");
-            container.appendChild(titleNode);
-            container.appendChild(elem);
-            newData.appendChild(container);
-            let dataArea = document.querySelector(".data");
-            topLevel.replaceChild(newData, dataArea);
-        })
-        .catch(err => {
-          console.log(err);
-        });
+  let topLevel = document.querySelector("body");
+  let newData = document.createElement("div");
+  newData.classList.add("data");
+  var pokemonID = getRandomInt(1, 800);
+  displayRandomPokemon(pokemonID, newData);
+  pokemonID = getRandomInt(1, 800);
+  displayRandomPokemon(pokemonID, newData);
 }
 
 // return random int
